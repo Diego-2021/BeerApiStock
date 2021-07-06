c29f64e30faac50c4b347505355b79cc9c10a8bf
Diego-2021 <delossantos.diego@gmail.com>
2021-07-06 13:32:34 -0300

    First commit

---
+++ beerapiStock/src/main/java/one/digitalinnovation/BeerStockApi/BeerStockApplication.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.BeerStockApi;
+
+import org.springframework.boot.SpringApplication;
+
+public class BeerStockApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run ( BeerStockApplication.class, args );
+    }
+
+}
